4bd89212bd218e07e23819098811d308543c73a9
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-06-01 19:50:48 -0500

    acquired axios; using json placeholder temporarily to learn; will replace with primary API later

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import React, { useState, useEffect } from "react";
 import SearchBar from "./components/search-bar/search-bar.component";
 
-function App() {
+import { client } from "./data/axios";
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    client.get("?_limit=10").then((response) => {
+      setPosts(response.data);
+    });
+  }, []);
+
+  const renderPosts = posts.map((post) => {
+    return (
+      <div className="post-card" key={post.id}>
+        <h2 className="post-title">{post.title}</h2>
+        <p className="post-body">{post.body}</p>
+        <div className="button">
+          <div className="delete-btn">Delete</div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
       <SearchBar />
+      <h2>Rendered Posts</h2>
+      {renderPosts}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const SearchBar = () => {
   return (
     <div className="ui segment">
